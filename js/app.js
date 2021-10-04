7c29183dc1ac5ab14ed7c9ad47bb6c7461018022
Andres <garzamorada@gmail.com>
2021-10-04 12:05:34 -0300

    filtros

--- js/app.js
+++ js/app.js
@@ -2,14 +2,19 @@
 /*       muestra oculta capa login        */
 /******************************************/
 
-function abre_cierra(op) {
-    if (op == 'cerrar') {
-        var elemento = document.getElementById('formulario_flotante');
-        elemento.className = 'formulario_flotante oculto';
-    } else if (op == 'abrir') {
-        var elemento = document.getElementById('formulario_flotante');
-        elemento.className = 'formulario_flotante mostrar';
+function abre_cierra(op, ID) {
+    if (op == 'ocultar') {
+        var elemento = document.getElementById(ID);
+        elemento.className = ID + ' ocultar';
+    } else if (op == 'mostrar') {
+        var elemento = document.getElementById(ID);
+        elemento.className = ID + ' mostrar';
     }
+}
+
+function cambia_clase(clase, ID) {
+    var elemento = document.getElementById(ID);
+    elemento.className = clase;
 }
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
@@ -67,4 +72,46 @@ var dia = getDD(fecha);
 var anio = fecha.getFullYear() - 18;
 var fechaMaxima = (anio + "-" + mes + "-" + dia);
 var campoFecha = document.getElementById('validationCustomNacimiento');
-campoFecha.max = fechaMaxima;
+if (campoFecha != null) { campoFecha.max = fechaMaxima; }
+
+
+/******************************************/
+/*        filtro de peliculas             */
+/******************************************/
+
+function ocultarClase(elementos) {
+    var i;
+    for (i = 0; i < elementos.length; i++) {
+        elementos[i].classList.remove('movie-mostrar');
+        elementos[i].classList.add('movie-ocultar');
+        console.log(elementos[i].className);
+    }
+}
+
+function mostrarClase(elementos) {
+    var i;
+    for (i = 0; i < elementos.length; i++) {
+        elementos[i].classList.remove('movie-ocultar');
+        elementos[i].classList.add('movie-mostrar');
+        console.log(elementos[i].className);
+    }
+}
+
+function seleccionaPeliculas(categoria) {
+    console.log(categoria);
+    switch (categoria) {
+        case 'marvel':
+            var elementos1 = document.getElementsByClassName("peliculas");
+            ocultarClase(elementos1);
+            var elementos2 = document.getElementsByClassName("marvel");
+            mostrarClase(elementos2);
+            break;
+        case 'todas':
+            var elementos1 = document.getElementsByClassName("peliculas");
+            mostrarClase(elementos1);
+            break;
+        default:
+            var elementos1 = document.getElementsByClassName("peliculas");
+            mostrarClase(elementos1);
+    }
+}
